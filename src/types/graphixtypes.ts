a5412f05e267594348594d5fd77d16b6291fe8e6
mustafakumshi <kumshimustafa0@gmail.com>
2025-02-05 15:39:11 +0530

    Add Types for Data Structure

---
+++ src/types/graphixtypes.ts
@@ -0,0 +1,21 @@
+export interface Node {
+  id: string;
+  position: { x: number; y: number };
+  data: {
+    label: string;
+    color: string;
+    fontSize: number;
+  };
+}
+
+export interface Edge {
+  id: string;
+  source: string;
+  target: string;
+}
+
+export interface HistoryState {
+  past: { nodes: Node[]; edges: Edge[] }[];
+  present: { nodes: Node[]; edges: Edge[] };
+  future: { nodes: Node[]; edges: Edge[] }[];
+}
